ff7a60ed0ff9587541bc51a9d82e53c39605bbb5
Nestorian <ananiev.nestor@ukr.net>
2025-05-11 21:19:51 +0300

    feat: finish chats

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,123 +1,219 @@
-import {Body, Controller, Get, Param, ParseIntPipe, Post, Put, Query, UseGuards} from '@nestjs/common';
-import {ChatService} from "./chat.service";
-import {JwtAuthGuard} from "../auth/jwt-auth.guard";
-import {RolesGuard} from "../auth/roles.guard";
-import {ApiBearerAuth, ApiBody, ApiQuery, ApiTags} from "@nestjs/swagger";
-import {GetUser} from "../common/decorators/get-user.decorator";
-import {Roles} from "../auth/roles.decorator";
-import {SendChatMessageDto} from "./dto/SendChatMessageDto";
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { ChatService } from './chat.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { RolesGuard } from '../auth/roles.guard';
+import { ApiBearerAuth, ApiBody, ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { GetUser } from '../common/decorators/get-user.decorator';
+import { Roles } from '../auth/roles.decorator';
+import { SendChatMessageDto } from './dto/SendChatMessageDto';
 
 @Controller('chat')
 @UseGuards(JwtAuthGuard, RolesGuard)
 @ApiBearerAuth()
 export class ChatController {
-    constructor(private readonly service: ChatService) {}
+  constructor(private readonly service: ChatService) {}
 
-    @Get('user/active')
-    @Roles('user')
-    @ApiTags('User chat')
-    @ApiQuery({
-        name: 'limit',
-        required: false,
-        type: Number,
-        example: 20,
-    })
-    @ApiQuery({
-        name: 'offset',
-        required: false,
-        type: Number,
-        example: 0,
-    })
-    async getActiveUserChat(
-        @GetUser('id') userId: number,
-        @Query('limit', ParseIntPipe) limit = 20,
-        @Query('offset', ParseIntPipe) offset = 0,
-    ) {
-        return this.service.getActiveUserChat(userId, limit, offset);
-    }
+  @Get('user/active')
+  @Roles('user')
+  @ApiTags('User chat')
+  @ApiQuery({
+    name: 'limit',
+    required: false,
+    type: Number,
+    example: 20,
+  })
+  @ApiQuery({
+    name: 'offset',
+    required: false,
+    type: Number,
+    example: 0,
+  })
+  async getActiveUserChat(
+    @GetUser('id') userId: number,
+    @Query('limit', ParseIntPipe) limit = 20,
+    @Query('offset', ParseIntPipe) offset = 0,
+  ) {
+    return this.service.getActiveUserChat(userId, limit, offset);
+  }
 
-    @Post('user/active')
-    @Roles('user')
-    @ApiTags('User chat')
-    @ApiBody({ type: SendChatMessageDto })
-    async sendMessage(
-        @GetUser('id') userId: number,
-        @Body() dto: SendChatMessageDto,
-    ) {
-        return this.service.sendUserMessage(userId, dto.text);
-    }
+  @Post('user/active')
+  @Roles('user')
+  @ApiTags('User chat')
+  @ApiBody({ type: SendChatMessageDto })
+  async sendMessage(
+    @GetUser('id') userId: number,
+    @Body() dto: SendChatMessageDto,
+  ) {
+    return this.service.sendUserMessage(userId, dto.text);
+  }
 
-    @Get('user/active/unread-count')
-    @Roles('user')
-    @ApiTags('User chat')
-    async getActiveUserChatUnreadCount(
-        @GetUser('id') userId: number,
-    ) {
-        return await this.service.getActiveUserChatUnreadCount(userId);
-    }
+  @Get('user/active/unread-count')
+  @Roles('user')
+  @ApiTags('User chat')
+  async getActiveUserChatUnreadCount(@GetUser('id') userId: number) {
+    return await this.service.getActiveUserChatUnreadCount(userId);
+  }
 
-    @Put('user/active/read')
-    @Roles('user')
-    @ApiTags('User chat')
-    async markChatAsRead(
-        @GetUser('id') userId: number,
-    ) {
-        return await this.service.markUserChatAsRead(userId);
-    }
+  @Put('user/active/read')
+  @Roles('user')
+  @ApiTags('User chat')
+  async markChatAsRead(@GetUser('id') userId: number) {
+    return await this.service.markUserChatAsRead(userId);
+  }
 
-    @Get('moderator/unread-counts')
-    @Roles('moderator')
-    @ApiTags('Moderator chat')
-    async getModeratorChatsUnreadCounts(
-        @GetUser('id') moderatorId: number,
-    ) {
-        return await this.service.getModeratorChatsUnreadCounts(moderatorId);
-    }
+  @Get('moderator/unread-counts')
+  @Roles('moderator')
+  @ApiTags('Moderator chat')
+  async getModeratorChatsUnreadCounts(@GetUser('id') moderatorId: number) {
+    return await this.service.getModeratorChatsUnreadCounts(moderatorId);
+  }
 
-    @Get('moderator/chats')
-    @Roles('moderator')
-    @ApiTags('Moderator chat')
-    @ApiQuery({
-        name: 'limit',
-        required: false,
-        type: Number,
-        example: 20,
-    })
-    @ApiQuery({
-        name: 'offset',
-        required: false,
-        type: Number,
-        example: 0,
-    })
-    @ApiQuery({
-        name: 'type',
-        required: true,
-        type: String,
-        example: "assigned"
-    })
-    @ApiQuery({
-        name: 'search',
-        required: false,
-        type: String,
-        example: ""
-    })
-    async getModeratorChats(
-        @GetUser('id') moderatorId: number,
-        @Query('limit', ParseIntPipe) limit = 20,
-        @Query('offset', ParseIntPipe) offset = 0,
-        @Query('type') type: "assigned" | "general" | "closed",
-        @Query('search') search: string = "",
-    ) {
-        return await this.service.getModeratorChats(moderatorId, limit, offset, type, search);
-    }
+  @Get('moderator/chats')
+  @Roles('moderator')
+  @ApiTags('Moderator chat')
+  @ApiQuery({
+    name: 'limit',
+    required: false,
+    type: Number,
+    example: 20,
+  })
+  @ApiQuery({
+    name: 'offset',
+    required: false,
+    type: Number,
+    example: 0,
+  })
+  @ApiQuery({
+    name: 'type',
+    required: true,
+    type: String,
+    example: 'assigned',
+  })
+  @ApiQuery({
+    name: 'search',
+    required: false,
+    type: String,
+    example: '',
+  })
+  async getModeratorChats(
+    @GetUser('id') moderatorId: number,
+    @Query('limit', ParseIntPipe) limit = 20,
+    @Query('offset', ParseIntPipe) offset = 0,
+    @Query('type') type: 'assigned' | 'general' | 'closed',
+    @Query('search') search: string = '',
+  ) {
+    return await this.service.getModeratorChats(
+      moderatorId,
+      limit,
+      offset,
+      type,
+      search,
+    );
+  }
 
-    @Put('moderator/chats/{id}/read')
-    @Roles('moderator')
-    @ApiTags('Moderator chat')
-    async markModeratorChatAsRead(
-        @GetUser('id') moderatorId: number,
-        @Param('id', ParseIntPipe) chatId: number,
-    ) {
-        return await this.service.markModeratorChatAsRead(moderatorId, chatId);
-    }
+  @Put('moderator/:id/read')
+  @Roles('moderator')
+  @ApiTags('Moderator chat')
+  async markModeratorChatAsRead(
+    @GetUser('id') moderatorId: number,
+    @Param('id', ParseIntPipe) chatId: number,
+  ) {
+    return await this.service.markModeratorChatAsRead(moderatorId, chatId);
+  }
+
+  @Get('moderator/:chatId/messages')
+  @Roles('moderator')
+  @ApiTags('Moderator chat')
+  @ApiQuery({
+    name: 'limit',
+    required: false,
+    type: Number,
+    example: 20,
+  })
+  @ApiQuery({
+    name: 'offset',
+    required: false,
+    type: Number,
+    example: 0,
+  })
+  async getModeratorChatMessages(
+    @GetUser('id') moderatorId: number,
+    @Param('chatId', ParseIntPipe) chatId: number,
+    @Query('limit', ParseIntPipe) limit = 20,
+    @Query('offset', ParseIntPipe) offset = 0,
+  ) {
+    return this.service.getModeratorChatMessages(
+      moderatorId,
+      chatId,
+      limit,
+      offset,
+    );
+  }
+
+  @Post('moderator/:chatId/messages')
+  @Roles('moderator')
+  @ApiTags('Moderator chat')
+  @ApiBody({ type: SendChatMessageDto })
+  async sendModeratorMessage(
+    @GetUser('id') moderatorId: number,
+    @Param('chatId', ParseIntPipe) chatId: number,
+    @Body() dto: SendChatMessageDto,
+  ) {
+    return this.service.sendModeratorMessage(moderatorId, chatId, dto.text);
+  }
+
+  @Post('moderator/:chatId/close')
+  @Roles('moderator')
+  @ApiTags('Moderator chat')
+  @ApiParam({
+    name: 'chatId',
+    required: true,
+    type: Number,
+  })
+  async closeChat(
+    @GetUser('id') moderatorId: number,
+    @Param('chatId', ParseIntPipe) chatId: number,
+  ) {
+    return this.service.closeChat(moderatorId, chatId);
+  }
+
+  @Put('moderator/:chatId/assign')
+  @Roles('moderator')
+  @ApiTags('Moderator chat')
+  @ApiParam({
+    name: 'chatId',
+    required: true,
+    type: Number,
+  })
+  async assignChat(
+    @GetUser('id') moderatorId: number,
+    @Param('chatId', ParseIntPipe) chatId: number,
+  ) {
+    return this.service.changeChatAssignee(moderatorId, moderatorId, chatId);
+  }
+
+  @Put('moderator/:chatId/unassign')
+  @Roles('moderator')
+  @ApiTags('Moderator chat')
+  @ApiParam({
+    name: 'chatId',
+    required: true,
+    type: Number,
+  })
+  async unassignChat(
+    @GetUser('id') moderatorId: number,
+    @Param('chatId', ParseIntPipe) chatId: number,
+  ) {
+    return this.service.changeChatAssignee(moderatorId, null, chatId);
+  }
 }
--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,99 +1,117 @@
 import {
-    ConnectedSocket,
-    MessageBody,
-    OnGatewayDisconnect,
-    SubscribeMessage,
-    WebSocketGateway,
-    WebSocketServer,
+  ConnectedSocket,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { BaseGateway } from '../common/base/base.gateway';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
-import { ChatService } from './chat.service';
-import {Role} from "../common/enum/Role";
-import {Repository} from "typeorm";
-import {ChatEntity} from "../common/entities/ChatEntity";
-import {ModeratorChatMessageEventDto} from "./dto/ModeratorChatMessageEventDto";
-import {InjectRepository} from "@nestjs/typeorm";
-import {ChatMessageEntity} from "../common/entities/ChatMessageEntity";
+import { Role } from '../common/enum/Role';
+import { Repository } from 'typeorm';
+import { ChatEntity } from '../common/entities/ChatEntity';
+import { ModeratorChatMessageEventDto } from './dto/ModeratorChatMessageEventDto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ChatMessageEntity } from '../common/entities/ChatMessageEntity';
+import { ChatMessageDto } from './dto/ChatMessageDto';
 type ChatSocket = Socket & { data: { user?: { id: number } } };
 
 @WebSocketGateway({ cors: true, namespace: 'chats' })
 export class ChatGateway extends BaseGateway {
-    @WebSocketServer()
-    server: Server;
+  @WebSocketServer()
+  server: Server;
 
-    private GENERAL_CHATS_ROOM = "chat:moderators:general";
+  private GENERAL_CHATS_ROOM = 'chat:moderators:general';
 
-    constructor(
-        jwtService: JwtService,
-        configService: ConfigService,
-        @InjectRepository(ChatEntity)
-        private readonly chatRepository: Repository<ChatEntity>,
-        @InjectRepository(ChatMessageEntity)
-        private readonly chatMessageRepository: Repository<ChatMessageEntity>,
-    ) {
-        super(jwtService, configService, [Role.User, Role.Moderator]);
+  constructor(
+    jwtService: JwtService,
+    configService: ConfigService,
+    @InjectRepository(ChatEntity)
+    private readonly chatRepository: Repository<ChatEntity>,
+    @InjectRepository(ChatMessageEntity)
+    private readonly chatMessageRepository: Repository<ChatMessageEntity>,
+  ) {
+    super(jwtService, configService, [Role.User, Role.Moderator]);
+  }
+
+  private getUserRoomName(userId: number): string {
+    return `chat:user:${userId}`;
+  }
+
+  private getModeratorChatRoomName(moderatorId: number): string {
+    return `chat:moderator:${moderatorId}`;
+  }
+
+  @SubscribeMessage('join_user_chat')
+  async handleUserJoin(@ConnectedSocket() client: ChatSocket) {
+    const userId = client.data.user?.id;
+    if (!userId || client.data.user?.role !== Role.User) {
+      return client.emit('error', 'Unauthorized');
+    }
+    await client.join(this.getUserRoomName(userId));
+  }
+
+  @SubscribeMessage('join_moderator_chat')
+  async handleModeratorJoin(@ConnectedSocket() client: ChatSocket) {
+    const moderatorId = client.data.user?.id;
+    if (!moderatorId || client.data.user?.role !== Role.Moderator) {
+      return client.emit('error', 'Unauthorized');
+    }
+    await client.join(this.GENERAL_CHATS_ROOM);
+    await client.join(this.getModeratorChatRoomName(moderatorId));
+  }
+
+  async emitUserMessage(newMessage: ChatMessageEntity) {
+    const chat = await this.chatRepository.findOne({
+      where: { user: { userId: newMessage.chat.user.userId }, isActive: true },
+      relations: ['assignedModerator'],
+    });
+
+    if (!chat) {
+      return;
     }
 
-    private getUserRoomName(userId: number): string {
-        return `chat:user:${userId}`;
-    }
+    const messagesCount = await this.chatMessageRepository.count({
+      where: { chat: { chatId: chat?.chatId } },
+    });
 
-    private getModeratorChatRoomName(moderatorId: number): string {
-        return `chat:moderator:${moderatorId}`;
-    }
+    const message: ModeratorChatMessageEventDto = {
+      is_assigned: chat.assignedModerator !== null,
+      is_new_chat: messagesCount == 1,
+      chat_id: chat.chatId,
+      user_full_name:
+        newMessage.chat.user.firstName + ' ' + newMessage.chat.user.lastName,
+      chat_message_id: newMessage.chatMessageId,
+      is_written_by_moderator: false,
+      text: newMessage.text,
+      sent_date: newMessage.sentDate,
+    };
+    const room = chat.assignedModerator
+      ? this.getModeratorChatRoomName(chat.assignedModerator.moderatorId)
+      : this.GENERAL_CHATS_ROOM;
+    this.server.to(room).emit('new_message', message);
+  }
 
-    @SubscribeMessage('join_user_chat')
-    async handleUserJoin(
-        @ConnectedSocket() client: ChatSocket,
-    ) {
-        const userId = client.data.user?.id;
-        if (!userId || client.data.user?.role !== Role.User) {
-            return client.emit('error', 'Unauthorized');
-        }
-        client.join(this.getUserRoomName(userId));
-    }
+  emitUserChatClosedMessage(userId: number) {
+    const room = this.getUserRoomName(userId);
+    this.server.to(room).emit('chat_closed');
+  }
 
-    @SubscribeMessage('join_moderator_chat')
-    async handleModeratorJoin(
-        @ConnectedSocket() client: ChatSocket,
-    ) {
-        const moderatorId = client.data.user?.id;
-        if (!moderatorId || client.data.user?.role !== Role.Moderator) {
-            return client.emit('error', 'Unauthorized');
-        }
-        client.join(this.GENERAL_CHATS_ROOM);
-        client.join(this.getModeratorChatRoomName(moderatorId));
-    }
+  emitModeratorMessage(newMessage: ChatMessageEntity) {
+    const message: ChatMessageDto = {
+      chat_message_id: newMessage.chatMessageId,
+      is_written_by_moderator: true,
+      text: newMessage.text,
+      sent_date: newMessage.sentDate,
+    };
+    const room = this.getUserRoomName(newMessage.chat.user.userId);
+    this.server.to(room).emit('new_message', message);
+  }
 
-   async emitUserMessage(newMessage: ChatMessageEntity) {
-
-       const chat = await this.chatRepository.findOne({
-           where: { user: { userId: newMessage.chat.user.userId }, isActive: true },
-           relations: ['assignedModerator']
-       });
-
-       if (!chat) {
-           return;
-       }
-
-       const messagesCount = await this.chatMessageRepository.count({
-           where: { chat: { chatId: chat?.chatId } }
-       });
-
-       const message: ModeratorChatMessageEventDto = {
-           is_new_chat: messagesCount == 1,
-           chat_id: chat.chatId,
-           user_full_name: newMessage.chat.user.firstName + " " + newMessage.chat.user.lastName,
-           chat_message_id: newMessage.chatMessageId,
-           is_written_by_moderator: false,
-           text: newMessage.text,
-           sent_date: newMessage.sentDate,
-       }
-       const room = chat.assignedModerator ?
-           this.getModeratorChatRoomName(chat.assignedModerator.moderatorId) : this.GENERAL_CHATS_ROOM;
-       this.server.to(room).emit('new_message', message);
-   }
+  emitGeneralChatAssigmentChangeMessage(chatId: number, isAssigned: boolean) {
+    this.server
+      .to(this.GENERAL_CHATS_ROOM)
+      .emit('assignment_change', { chatId, isAssigned });
+  }
 }
