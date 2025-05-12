1a94218851a9eb2e619273089541c0484236719d
NureHlobaMaksym <maksym.hloba1@nure.ua>
2025-05-12 17:07:47 +0300

    feat: admin requests

--- src/user/user.mapper.ts
+++ src/user/user.mapper.ts
@@ -10,6 +10,9 @@ export class UserMapper {
       first_name: user.firstName,
       last_name: user.lastName,
       has_premium: user.userPremium?.has_premium ?? false,
+      email: user.email,
+      registered_at: user.registeredAt,
+      is_temporary_password: user.isTemporaryPassword,
     };
   }
 }
