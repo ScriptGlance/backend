fe69f4dcd0113432d2ec1929dcc92262e09625aa
Nestorian <ananiev.nestor@ukr.net>
2025-05-31 03:23:19 +0300

    feat: add avatar to chat dto

--- src/chat/dto/ChatDto.ts
+++ src/chat/dto/ChatDto.ts
@@ -1,6 +1,7 @@
 export class ChatDto {
   chat_id: number;
   user_full_name: string;
+  avatar: string | null;
   last_message: string;
   last_message_sent_date: Date;
   unread_messages_count: number;
