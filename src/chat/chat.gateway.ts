541112cf6458317cb7d5b33779f123f4329b57b6
Yehor Bielik <yehor.bielik@nure.ua>
2025-06-01 11:51:57 +0300

    fix: moderator chat message user avatar

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -15,7 +15,8 @@ import { ModeratorChatMessageEventDto } from './dto/ModeratorChatMessageEventDto
 import { InjectRepository } from '@nestjs/typeorm';
 import { ChatMessageEntity } from '../common/entities/ChatMessageEntity';
 import { ChatMessageDto } from './dto/ChatMessageDto';
-import {ChatDto} from "./dto/ChatDto";
+import { ChatDto } from './dto/ChatDto';
+
 type ChatSocket = Socket & { data: { user?: { id: number } } };
 
 @WebSocketGateway({ cors: true, namespace: 'chats' })
@@ -83,7 +84,9 @@ export class ChatGateway extends BaseGateway {
       chat_id: chat.chatId,
       user_full_name:
         newMessage.chat.user.firstName + ' ' + newMessage.chat.user.lastName,
-      avatar: newMessage.chat.user.avatar,
+      avatar: newMessage.chat.user.avatar
+        ? '/' + newMessage.chat.user.avatar.replace('uploads/', '')
+        : undefined,
       chat_message_id: newMessage.chatMessageId,
       is_written_by_moderator: false,
       text: newMessage.text,
